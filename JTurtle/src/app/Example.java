02a56ab7eaeaaefe9daa1132e7f93f7074a4b463
cvetan <cgg@abv.bg>
2017-07-06 09:00:13 +0300

    JTurtle Working

--- JTurtle/src/app/Example.java
+++ JTurtle/src/app/Example.java
@@ -1,144 +1,202 @@
 package app;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import ch.aplu.turtle.Playground;
 import ch.aplu.turtle.Turtle;
-import ch.aplu.turtle.TurtleContainer;
 import ch.aplu.turtle.TurtleFrame;
 
 public class Example {
-	
-	private JFrame mainFrame;
-	private JLabel headerLabel;
-	private JLabel statusLabel;
+
 	private JPanel controlPanel;
-	private JPanel turtlePanel;
-	private JLabel msglabel;
-	
+	private JPanel panel;
+	private Boolean check = false;
+
 	private TurtleFrame myTFrame;
+	private Turtle joe;
 	
-	private Playground myPlayGround;
-	
-	public Example(){
-	      prepareGUI();
-	   }
-	
-	public static void main (String[] args) {
-		
-		Example myExample = new Example();
+	private final Thread t;
+	{
+		t = new Thread(new Runnable() {
+			public void run() {
+				while (!Thread.interrupted()) {
+					try {
+						if (check) {
+							drawRect();
+							Thread.sleep(1000);
+//							Thread.sleep(1 * 60 * 1000);
+						}
+					} catch (Throwable t) {
+						t.printStackTrace();
+					}
+				}
+			}
+		});
+		t.setDaemon(true);
+		t.setName("Draw");
+		t.start();
 	}
 	
-	public void prepareGUI() {
-//		mainFrame = new JFrame("Java Swing Examples");
-//		mainFrame.setSize(800, 800);
-//		mainFrame.setVisible(true);
-		
-//		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-//		mainFrame.setLayout(new GridLayout(1, 2));
-		
-//		Container contentPane = frame.getContentPane();
-		
-//		contentPane.add(aComponent);
-		
-//		JButton closeButton  = new JButton("Close");
-		
-//		contentPane.add(closeButton);
-		
-//		frame.setBounds(50, 50, 300, 200);
-//		frame.setLayout(new FlowLayout());
-//		frame.pack();
-		
+	private Example(){
+		prepareGUI();
+	}
+
+	public static void main (String[] args) {
+		Example myExample = new Example();
+	}
+
+	class CustomActionListenerDraw implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			check=true;
+		}
+	}
+
+	class CustomActionListenerReset implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			resetTurtle();
+		}
+	}
+
+	class CustomActionListenerHide implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			joe.hideTurtle();
+		}
+	}
+
+	class CustomActionListenerShow implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			joe.showTurtle();
+		}
+	}
+
+	private void resetTurtle() {
+		joe.home();
+		myTFrame.getPlayground().clear();
+	}
+	
+	private void drawRect() {
+		joe.showTurtle();
+		joe.setPos(-100, 100);                 // Place joe to the Point(-100,100).
+		joe.setPenColor(Color.blue);
+		for (int i = 0; i < 4; i++) {
+			joe.rt(90).fd(200);                 // turn 90 degrees to the right, then 
+												// move forward 200 pixels.
+		}
+		joe.setPenColor(Color.red);           // set the pen color to red.
+		joe.pu();                             // lifts the pen off the canvas
+		joe.bk(50).lt(90).bk(50).rt(90).pd();
+		for (int i = 0; i < 4; i++) {
+			joe.rt(90).fd(100);
+		}
+		check=false;
+	}
+
+	private void prepareGUI() {
+
 		controlPanel = new JPanel();
-	    /*controlPanel.setLayout(new FlowLayout());
-	    
-	    JButton okButton = new JButton("OK");        
-	    JButton javaButton = new JButton("Submit");
-	    JButton cancelButton = new JButton("Cancel");
-	    
-	    controlPanel.add(okButton);
-	    controlPanel.add(javaButton);
-	    controlPanel.add(cancelButton);*/
-		
-		JPanel panel = new JPanel();
-	      
-	      // panel.setBackground(Color.darkGray);
-//	      panel.setSize(200,200);
-	      GroupLayout layout = new GroupLayout(panel);
-	      layout.setAutoCreateGaps(true);
-	      layout.setAutoCreateContainerGaps(true);
-	      
-	      JButton btn1 = new JButton("Button 1");
-	      JButton btn2 = new JButton("Button 2");
-	      JButton btn3 = new JButton("Button 3");
+		panel = new JPanel();
 
-	      layout.setHorizontalGroup(layout.createSequentialGroup()
-	        .addGroup(layout.createSequentialGroup()
-	        .addGroup(layout.createParallelGroup(
-	        GroupLayout.Alignment.LEADING)
-	        .addComponent(btn1)
-	        .addComponent(btn2)
-	        .addComponent(btn3))));
-	   
-	      layout.setVerticalGroup(layout.createSequentialGroup()
-	         .addComponent(btn1)
-	         .addComponent(btn2)
-	         .addComponent(btn3));
-	      
-	      panel.setLayout(layout);
-//	      controlPanel.add(panel);
+		GroupLayout layout = new GroupLayout(panel);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		Dimension d = new Dimension(150, 20);
+		
+		JButton btn1 = new JButton("Rectangular");
+		btn1.addActionListener(new CustomActionListenerDraw());
 
-//	    mainFrame.add(controlPanel);
-		 
-//		Dimension size = new Dimension(500, 800);
+		JButton btn2 = new JButton("Reset");
+		btn2.addActionListener(new CustomActionListenerReset());
+
+		JButton btn3 = new JButton("Hide");
+		btn3.addActionListener(new CustomActionListenerHide());
+
+		JButton btn4 = new JButton("Show");
+		btn4.addActionListener(new CustomActionListenerShow());
 		
+		List<JButton> bList = new ArrayList<>();
+		bList.add(btn1);
+		bList.add(btn2);
+		bList.add(btn3);
+		bList.add(btn4);
 		
-//		TurtleFrame myTFrame = new TurtleFrame("frame_01", myPlayGround);
+		for (JButton jButton : bList) {
+			jButton.setMinimumSize(d);
+		}
+		
+		layout.setHorizontalGroup(layout.createSequentialGroup()
+				.addGroup(layout.createSequentialGroup()
+						.addGroup(layout.createParallelGroup(
+								GroupLayout.Alignment.CENTER)
+								.addComponent(btn1)
+								.addComponent(btn2)
+								.addComponent(btn3)
+								.addComponent(btn4)
+								)));
+
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addComponent(btn1)
+				.addComponent(btn2)
+				.addComponent(btn3)
+				.addComponent(btn4)
+				);
+		
+		panel.setLayout(layout);
+		controlPanel.add(panel);
+		
 		myTFrame = new TurtleFrame("MyTurtles", 1000, 800);
-		myTFrame.setLayout(new GridLayout(1, 2));
-//		myTFrame.setVisible(true);
-//		Playground myPlayGround = new Playground(myTFrame, size);
-//		myPlayGround = myTFrame.getPlayground();
-//		mainFrame.add(myPlayGround);
-//		mainFrame.add(myTFrame);
-//		turtlePanel = myPlayGround.get
 		
-		myTFrame.add(controlPanel);
-		myTFrame.add(panel);
+		myTFrame.add(controlPanel, BorderLayout.EAST);
 		
-//		TurtleContainer myPlayGround = new TurtleContainer(myPlayGround, size);
-		
-//		myTFrame.setLayout(new FlowLayout());
-//		myPlayGround.add(closeButton);
-		 
-		    Turtle joe = new Turtle(myTFrame, Color.green); // Create a green turtle in her 
-		                                          //own window.
-		    joe.setPos(-100, 100);                 // Place joe to the Point(-100,100).
-		    
-		    for (int i=0; i < 4; i++) {
-		      joe.rt(90).fd(200);                 // turn 90 degrees to the right, then 
-		                                          // move forward 200 pixels.
-		    }
-		    joe.setPenColor(Color.red);           // set the pen color to red.
-		    joe.pu();                             // lifts the pen off the canvas
-		    joe.bk(50).lt(90).bk(50).rt(90).pd();
-		    for (int i=0; i < 4; i++) {
-		      joe.rt(90).fd(100);
-		    }
-		    
-		    /*
-		    Turtle anne = new Turtle(joe);        // Create a new Turtle (named anne)
+		joe = new Turtle(myTFrame, Color.green); // Create a green turtle in her 
+		//own window.
+		joe.hideTurtle();
+		joe.setLineWidth(2);
+		myTFrame.setVisible(true);
+		/*
+		BorderLayout borderLayout = new BorderLayout();
+		borderLayout.setHgap(10);
+		borderLayout.setVgap(10);
+		myTFrame.setLayout(borderLayout);
+		 */
+
+		//		Playground myPlayGround = new Playground(myTFrame, size);
+		//		myPlayGround = myTFrame.getPlayground();
+		//		mainFrame.add(myPlayGround);
+		//		mainFrame.add(myTFrame);
+
+		//		myTFrame.add(controlPanel);
+
+		//		TurtleContainer myPlayGround = new TurtleContainer(myPlayGround, size);
+
+		//		myTFrame.setLayout(new FlowLayout());
+		//		myPlayGround.add(closeButton);
+
+		//	}
+
+		/*
+		    Turtle anne = new Turtle(joe, Color.BLUE);        // Create a new Turtle (named anne)
 		                                          // in the same window as joe
 		    anne.speed(1000).fd(150).lt(90);      // sets the speed to 1000 pixels/sec,
 		                                          // then do some moves.
@@ -148,13 +206,14 @@ public class Example {
 		                                          // screen
 		    anne.wrap();                          // Tells anne to wrap around the edges
 		    anne.setPos(200,200);
-		    
+
 		    for (int i=0; i < 4; i++) {
 		      anne.rt(90).fd(400);
 		    }
-		    
+
 		    anne.reinit();                        // Resets anne to her standard settings,
 		                                          // e.g. home position,facing north 
+
 		    Turtle filly = new Turtle(joe, Color.yellow); // yellow Turtle in joe's
 		                                          // Playground.
 		    filly.setPos(75,75);
@@ -168,10 +227,7 @@ public class Example {
 
 		    Turtle texter = new Turtle(joe, Color.magenta);
 		    texter.ht().label("Hello Turtle");
-		    */
-		  }
-	
-	
+		 */
+	}
 
-	 
 }
